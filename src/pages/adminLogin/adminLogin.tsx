acccf3a414567f6106edb39243ec4aff8fcd1dee
soyeoung1 <soyeoung06@gmail.com>
2025-08-23 21:46:15 +0900

    feat:admin login 개발 #1

---
+++ src/pages/adminLogin/adminLogin.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AdminLogin: React.FC = () => {
+  return (
+    <LoginContainer>
+       admin login
+    </LoginContainer>
+  );
+};
+
+export default AdminLogin;
+
+
+const LoginContainer = styled.div`
+
+`;
